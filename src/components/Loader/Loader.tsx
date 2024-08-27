a9dd46581f106c67a65cb818ddf545822d2df908
adocoder12 <adonay.jadal@gmail.com>
2024-08-27 11:44:40 +0300

    need responsive updates P V.2

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,5 @@
 const Loader = () => (
-  <div className="relative w-20 h-20 mx-auto grid place-items-center bottom-32">
+  <div className="relative w-20 h-20 mx-auto grid place-items-center ">
     <div className="absolute -top-8 left-0 w-full h-full bg-decorator rounded-lg animate-jump7456"></div>
     <div className="absolute bottom-4 right-5 w-12 h-1 shadow-lg bg-slate-200 rounded-full animate-shadow324"></div>
   </div>
