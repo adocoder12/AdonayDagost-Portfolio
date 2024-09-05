4a512f641c5e2d24238a03113ac3b3a406d953b4
adocoder12 <adonay.jadal@gmail.com>
2024-09-05 18:12:04 +0300

    editing css

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,7 @@
 const Loader = () => (
-  <div className="grid place-content-center min-h-[80vh]">
-    <div className="relative w-20 h-20 mx-auto grid place-items-center ">
-      <div className="absolute -top-8 left-0 w-full h-full bg-decorator rounded-lg animate-jump7456"></div>
-      <div className="absolute bottom-4 right-5 w-12 h-1 shadow-lg bg-slate-200 rounded-full animate-shadow324"></div>
-    </div>
+  <div className="relative w-20 h-20 mx-auto grid place-items-center ">
+    <div className="absolute -top-8 left-0 w-full h-full bg-decorator rounded-lg animate-jump7456"></div>
+    <div className="absolute bottom-4 right-5 w-12 h-1 shadow-lg bg-slate-200 rounded-full animate-shadow324"></div>
   </div>
 );
 
