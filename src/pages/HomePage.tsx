6bf1b45eb8eb679a67f64d2fd29ff0ff2e9464d7
adocoder12 <adonay.jadal@gmail.com>
2024-10-20 22:35:34 +0300

    fixing css skills component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -63,7 +63,7 @@ export default function HomePage({ services }: { services: TServices[] }) {
         <h2 className="text-xl   md:text-4xl font-semibold leading-tight">
           Mastering the Craft:
         </h2>
-        <p className="font-light text-lg lg:text-xl leading-loose  text-justify  ">
+        <p className="font-light text-lg lg:text-xl leading-8  text-justify  ">
           Innovation, creativity, and passion come together in the projects I’ve
           crafted. Each project reflects a commitment to excellence and a love
           for problem-solving, whether it's developing intuitive user
