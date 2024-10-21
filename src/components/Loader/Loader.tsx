57ee45fbe316e11c6e76fbdeadfb899075723be4
adocoder12 <adonay.jadal@gmail.com>
2024-10-21 16:08:27 +0300

    fix css line height

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,11 @@
-const Loader = () => (
-  <div className="relative w-20 h-20 mx-auto grid place-items-center ">
-    <div className="absolute -top-8 left-0 w-full h-full bg-decorator rounded-lg animate-jump7456"></div>
-    <div className="absolute bottom-4 right-5 w-12 h-1 shadow-lg bg-slate-200 rounded-full animate-shadow324"></div>
-  </div>
-);
+const Loader = () => {
+  return (
+    <div className="w-full gap-x-2 flex justify-center items-center">
+      <div className="w-5 bg-[#d991c2] h-5 rounded-full animate-bounce" />
+      <div className="w-5  h-5 bg-[#9869b8] rounded-full animate-bounce" />
+      <div className="w-5 h-5  bg-[#6756cc] rounded-full animate-bounce" />
+    </div>
+  );
+};
 
 export default Loader;
