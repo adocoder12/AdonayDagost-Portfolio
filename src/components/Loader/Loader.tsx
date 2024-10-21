67aa198a86d9fd61164c2a7c9073e6b1793c300a
adocoder12 <adonay.jadal@gmail.com>
2024-10-21 16:09:50 +0300

    fix css line height

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,8 @@
 const Loader = () => {
   return (
     <div className="w-full gap-x-2 flex justify-center items-center">
-      <div className="w-5 bg-[#d991c2] h-5 rounded-full animate-bounce" />
-      <div className="w-5  h-5 bg-[#9869b8] rounded-full animate-bounce" />
+      <div className="w-5 bg-decorator h-5 rounded-full animate-bounce" />
+      <div className="w-5  h-5 bg-mainColor rounded-full animate-bounce" />
       <div className="w-5 h-5  bg-[#6756cc] rounded-full animate-bounce" />
     </div>
   );
