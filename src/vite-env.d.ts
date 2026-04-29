21137017d3ae034f11617cce51273bfe071254b8
adocoder12 <adonay.jadal@gmail.com>
2026-04-29 20:30:36 +0300

    fix: manual redeploy with standard css

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
-declare module "*.module.css" {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module "*.css" {
   const content: string;
   export default content;
