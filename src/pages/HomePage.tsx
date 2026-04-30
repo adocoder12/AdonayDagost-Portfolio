efb0b4c7a1a6814dcfeecfb43e4393de3bcbe852
adocoder12 <adonay.jadal@gmail.com>
2026-04-30 12:18:10 +0300

    fix: padding to introduction

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -42,7 +42,7 @@ export default function HomePage({ services }: { services: TServices[] }) {
 
       <div className="container-sized">
         {/* Editorial Introduction */}
-        <section className="flex flex-col items-center justify-center text-center py-20 gap-y-10">
+        <section className="flex flex-col items-center justify-center text-center py-14 md:py-20 gap-y-10">
           <h2 className=" text-h5 md:text-h4 lg:text-h2 leading-relaxed text-black-02   md:leading-snug lg:leading-tight font-light text-content max-w-5xl italic">
             "My passion for creativity and problem-solving led me to explore
             frontend deve lopment. The moment I crafted my first webpage, I was
@@ -52,7 +52,7 @@ export default function HomePage({ services }: { services: TServices[] }) {
         </section>
 
         {/* Gallery Section */}
-        <section className="flex flex-col pt-20 gap-y-12 border-t border-white/5">
+        <section className="flex flex-col pt-14 md:pt-20 gap-y-12 border-t border-white/5">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
             <h2 className="text-4xl md:text-7xl font-black uppercase tracking-tighter text-black-02 leading-[0.85]">
               Mastering <br /> the Craft_
